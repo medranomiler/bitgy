6a1208240d9a61b2a0c5a182813452fc0a45000b
Medranomiler1 <medranomiler@gmail.com>
2023-06-16 13:07:01 -0500

    increased spacing on features and added logo tagline

--- src/Features.jsx
+++ src/Features.jsx
@@ -6,12 +6,14 @@ import Key from './assets/key.svg'
 
 const Features = () => {
     return (
-        <div className="md:min-h-screen min-h-[1000px] flex flex-col bg-slate-50 items-center justify-center overflow-hidden">
+        <div className="md:min-h-screen min-h-[1000px] flex flex-col bg-slate-50 items-center justify-center space-y-8 overflow-hidden">
+{/* Top half */}
             <div className="w-full h-1/2 md:w-3/4 flex flex-col md:flex-row items-center">
                 <div className="w-full md:w-1/2 h-3/4 flex flex-col items-center">
                     <div className="w-full md:w-13/4 h-full border-gray-400 p-8 ">
-                        <p className="font-bold text-gray-400 h-1/4">Save in Bitcoin</p>
-                        <h3 className='font-bold lg:text-4xl text-3xl h-1/2 mb-2'>A fun and easy way for anyone to learn about bitcoin while they accumulate and save with Bitgy Bank.</h3>
+                        <p className="font-bold text-gray-400 h-1/4 mb-2">Save in Bitcoin</p>
+                        <h3 className='font-bold lg:text-4xl text-3xl h-1/2'>A fun and easy way for anyone to learn about bitcoin while they accumulate and save with Bitgy Bank.</h3>
+                        <br/>
                         <p className='h-1/4'>Simply deposit sats to your Bitgy Bank by tapping  with an NFC enabled lightning wallet, or scan with your QR scanner.</p>
                         <br/>
                         <p className='h-1/4'>Make choress fun again by rewarding with clean sats! No more dirty fiat!</p>
@@ -23,14 +25,8 @@ const Features = () => {
                     </div>
                 </div>
             </div>
+{/* Bottom Half */}
             <div className="w-full h-1/2 md:w-3/4 flex flex-col md:flex-row items-center">
-            {/* <div className="w-full h-3/4 flex flex-col items-center">
-                    <div className="w-full md:w-13/4 h-full border-gray-400 p-8">
-                        <p className="font-bold text-gray-400 h-1/4">Self Custody</p>
-                        <h3 className='font-bold lg:text-4xl text-3xl h-1/2 mb-2'>You hold the keys to your Bitgy Bank - No third parties can access your funds</h3>
-                        <p className='h-1/4'>Take control of your savings by holding the private keys to your stash.</p>
-                    </div>
-                </div> */}
                 <div className="w-full md:w-1/2 h-full p-8 order-2 md:order-1">
                 <div className="flex justify-center items-center w-full h-full">
                     <img src={Bitcoin} className="md:h-48 md:w-auto w-[300px] h-auto -translate-x-8 " /><img src={Key} className="md:h-24 md:w-auto w-64 h-auto z-10" />
@@ -38,13 +34,14 @@ const Features = () => {
                 </div>
                 <div className="md:block md:w-1/2 h-3/4 flex flex-col items-center border-blue-400">
                     <div className="w-full md:w-13/4 h-full border-gray-400 p-8">
-                        <p className="font-bold text-gray-400 h-1/4">Self Custody</p>
-                        <h3 className='font-bold lg:text-4xl text-3xl h-1/2 mb-2'>You hold the keys to your Bitgy Bank - No third parties can access your funds</h3>
+                        <p className="font-bold text-gray-400 h-1/4 mb-2">Self Custody</p>
+                        <h3 className='font-bold lg:text-4xl text-3xl h-1/2'>You hold the keys to your Bitgy Bank - No third parties can access your funds</h3>
+                        <br/>
                         <p className='h-1/4'>Whether you are teaching the value of self custody or learning the value of self custody for the first time, Bitgy allows you to take control of your savings by holding the keys to your Bitcoin.</p>
                     </div>
                 </div>
-    
             </div>
+
         </div>
       )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -3,8 +3,8 @@ import Logo from './assets/logo.svg'
 
 const Header = () => {
   return (
-    <div className="w-screen h-auto p-4 bg-slate-50 flex justify-center">
-        <img src={Logo} className="w-3/4"/>
+    <div className="w-screen h-auto px-4 pt-4 bg-slate-50 flex justify-center pb-24">
+        <img src={Logo} className="md:w-3/4"/>
     </div>
   )
 }
