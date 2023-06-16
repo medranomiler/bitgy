ad9b331bc915932bad4fb8b27606ed3b09667cc8
Medranomiler1 <medranomiler@gmail.com>
2023-06-15 20:42:24 -0500

    updated text content, and some graphics

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,7 @@ const App = () => {
     <>
       <div className="bg-slate-50 w-auto py-6 flex justify-center items-center">
         <div className="flex-col justify-center items-center">
-          <div className="translate-x-24 md:translate-x-64"><img src={Bitcoin} className="w-16 h-auto md:w-64 animate-bounce" /></div>
-          <div className="translate-x-12"><img src={Bitcoin} className="w-16 h-auto md:w-64 animate-bounce" /></div>
-          <div className="translate-x-24 md:translate-x-64"><img src={Bitcoin} className="w-16 h-auto md:w-64 animate-bounce" /></div>
-          <div className="translate-x-12"><img src={Bitcoin} className="w-16 h-auto md:w-64 animate-bounce" /></div>
-          <div className="translate-x-24 md:translate-x-64"><img src={Bitcoin} className="w-16 h-auto md:w-64 animate-bounce" /></div>
+          <div className="translate-x-20 md:translate-x-48"><img src={Bitcoin} className="w-16 h-auto md:w-64 animate-bounce" /></div>
           <img src={Bitgy} className="w-64 h-auto md:w-[800px]" />
         </div>
       </div>
--- src/Features.jsx
+++ src/Features.jsx
@@ -6,13 +6,15 @@ import Key from './assets/key.svg'
 
 const Features = () => {
     return (
-        <div className="md:h-screen min-h-[1000px] flex flex-col bg-slate-50 items-center overflow-hidden">
+        <div className="md:min-h-screen min-h-[1000px] flex flex-col bg-slate-50 items-center justify-center overflow-hidden">
             <div className="w-full h-1/2 md:w-3/4 flex flex-col md:flex-row items-center">
                 <div className="w-full md:w-1/2 h-3/4 flex flex-col items-center">
                     <div className="w-full md:w-13/4 h-full border-gray-400 p-8 ">
                         <p className="font-bold text-gray-400 h-1/4">Save in Bitcoin</p>
-                        <h3 className='font-bold lg:text-4xl text-3xl h-1/2 mb-2'>Learn about bitcoin while you earn and save with Bitgy Bank</h3>
-                        <p className='h-1/4'>Simply earn or purchase bitcoin, and deposit sats to your Bitgy Bank by tapping its nose with an NFC enabled lightning wallet</p>
+                        <h3 className='font-bold lg:text-4xl text-3xl h-1/2 mb-2'>A fun and easy way for anyone to learn about bitcoin while they accumulate and save with Bitgy Bank.</h3>
+                        <p className='h-1/4'>Simply deposit sats to your Bitgy Bank by tapping  with an NFC enabled lightning wallet, or scan with your QR scanner.</p>
+                        <br/>
+                        <p className='h-1/4'>Make choress fun again by rewarding with clean sats! No more dirty fiat!</p>
                     </div>
                 </div>
                 <div className="w-full md:w-1/2 h-full p-8">
@@ -31,14 +33,14 @@ const Features = () => {
                 </div> */}
                 <div className="w-full md:w-1/2 h-full p-8 order-2 md:order-1">
                 <div className="flex justify-center items-center w-full h-full">
-                    <img src={Bitcoin} className="md:h-64 md:w-auto w-[300px] h-auto translate-x-4 -translate-y-4" /><img src={Key} className="md:h-24 md:w-auto w-64 h-auto animation-key" />
+                    <img src={Bitcoin} className="md:h-48 md:w-auto w-[300px] h-auto -translate-x-8 " /><img src={Key} className="md:h-24 md:w-auto w-64 h-auto z-10" />
                     </div>
                 </div>
                 <div className="md:block md:w-1/2 h-3/4 flex flex-col items-center border-blue-400">
                     <div className="w-full md:w-13/4 h-full border-gray-400 p-8">
                         <p className="font-bold text-gray-400 h-1/4">Self Custody</p>
                         <h3 className='font-bold lg:text-4xl text-3xl h-1/2 mb-2'>You hold the keys to your Bitgy Bank - No third parties can access your funds</h3>
-                        <p className='h-1/4'>Take control of your savings by holding the private keys to your stash.</p>
+                        <p className='h-1/4'>Whether you are teaching the value of self custody or learning the value of self custody for the first time, Bitgy allows you to take control of your savings by holding the keys to your Bitcoin.</p>
                     </div>
                 </div>
     
